05cfe57edf7de6ed59b896ec57b3bdc27ce45d9e
Tomaszunek <pawlowskitomaszu@gmail.com>
2019-12-16 23:02:52 +0100

    add year reducer

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -18,19 +18,7 @@ const Welcome: FunctionComponent<WelcomeProps> = props => {
 
   useEffect(() => {
     thunkSendMessage();
-  });
-
-  const columns = [
-    { key: 'id', name: 'ID' },
-    { key: 'title', name: 'Title' },
-    { key: 'count', name: 'Count' }
-  ];
-
-  const rows = [
-    { id: 0, title: 'row1', count: 20 },
-    { id: 1, title: 'row1', count: 40 },
-    { id: 2, title: 'row1', count: 60 }
-  ];
+  });  
 
   return (
     <Fragment>
