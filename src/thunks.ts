c6674f022592ad80c2ca60ee6a184cb4c106e360
Tomaszunek <pawlowskitomaszu@gmail.com>
2019-12-16 21:43:16 +0100

    add api endpoints

--- src/thunks.ts
+++ src/thunks.ts
@@ -6,18 +6,18 @@ import { IRootState } from './store';
 import { Counter } from './store/Counter/types';
 import { HOSTNAME_API } from './constants/routing';
 
+// function exampleAPI() {
+//   return Promise.resolve(5);
+// }
+
+const getFromApi = () => {
+  const data = axios.get(`${HOSTNAME_API}/api`);
+  return data;
+};
+
 export const thunkSendMessage = (
   counter: Counter
 ): ThunkAction<void, IRootState, null, Action> => async dispatch => {
   const asyncResp = await getFromApi();
   dispatch(INCRE({ count: asyncResp.data.item + counter.count }));
-};
-
-function exampleAPI() {
-  return Promise.resolve(5);
-}
-
-const getFromApi = () => {
-  const data = axios.get(`${HOSTNAME_API}/api`);
-  return data;
 };
